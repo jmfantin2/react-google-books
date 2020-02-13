7389673b519d7ee11d707331a8c038d1e0f830a3
JM Fantin <joao.fantin@acad.pucrs.br>
2020-02-13 19:16:32 -0300

    Book ID query handling in BookDetails

--- src/pages/BookDetails/index.js
+++ src/pages/BookDetails/index.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import { useLocation } from "react-router-dom";
+import axios from 'axios';
 
 import './index.css'
 
-export default function BookDetails(){
+function BookDetails () {
+  const [bookId, setBookId] = useState('');
+  const query = new URLSearchParams(useLocation().search);
+
+  useEffect(() => {
+    function retrieveBookId() {
+      setBookId(query.get("book_id"));
+    }
+    retrieveBookId();
+    getVolume();
+  });
+
+  function getVolume() {
+    axios.request({
+      method: 'get',
+      url: 'https://www.googleapis.com/books/v1/volumes/volumeId?q=' + bookId
+    }).then((response) => {
+      console.log('Response: ', response);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
   return(
-    <h1>BookDetails</h1>
+    <div>
+      <h1>BookDetails</h1>
+    </div>
   );
 }
+
+export default BookDetails;
